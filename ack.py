a593e0538dcef8efbc5fc42722eb8d221cb1b32b
Kyle Hart <krh@Kyles-MacBook-Pro.local>
2014-10-09 21:53:41 -0700

    Ackermanns Function

---
+++ ack.py
@@ -0,0 +1,14 @@
+def ackermann(m, n):
+    """Computes the Ackermann fuction for two values"""
+    if m < 0 or n < 0:
+        return None
+    else:
+        if m == 0:
+            return n + 1
+        elif m > 0 and n == 0:
+            return ackermann(m-1, 1)
+        elif m > 0 and n > 0:
+            return ackermann(m-1, ackermann(m, n-1))
+
+x = ackermann(1,2)
+print x 
