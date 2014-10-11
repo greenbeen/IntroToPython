ee7ee4c9398dffa7f7d0340943afc6faaa0387c3
Kyle Hart <krh@Kyles-MacBook-Pro.local>
2014-10-10 18:12:41 -0700

    added assertions

--- session02/ack.py
+++ session02/ack.py
@@ -10,7 +10,13 @@ def ackermann(m, n):
         elif m > 0 and n > 0:
             return ackermann(m-1, ackermann(m, n-1))
 
-x = ackermann(1,2)
-print x 
+#print ackermann(1,2)
 
-if __mame__ == "__main__"
+
+# Test function
+if __name__ == "__main__":
+    assert ackermann(0, 0) == 1
+    assert ackermann(1, 1) == 3
+    assert ackermann(2, 2) == 7
+    assert ackermann(3, 3) == 61
+    print "All tests passed"
